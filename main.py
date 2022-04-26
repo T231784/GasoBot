c84ec73952c082700bc67526e8bc27c1556767d4
T231784 <pulaskau@gmail.com>
2022-04-26 14:05:36 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from tkinter import image_names
 import config
 import discord
 
+#Take api token and permission link from config file
 TOKEN = config.TOKEN
 PERMISSIONLINK = config.permissionLink
+
+client = discord.Client()
+
+@client.event 
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+client.run(TOKEN)
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} on {channel}')
+    
