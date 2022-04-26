528dadd439af2e1296a3e27a9a2d438d1f8c962e
T231784 <pulaskau@gmail.com>
2022-04-26 17:56:57 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
+from pydoc import resolve
 import config
 import discord
 
-#Take api token and permission link from config file
+#get api token and permission link from config file 
 TOKEN = config.TOKEN
 PERMISSIONLINK = config.permissionLink
-
+        
 client = discord.Client()
-
+        
 @client.event 
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
-client.run(TOKEN)
-
 @client.event
 async def on_message(message):
+    
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
-    print(f'{username}: {user_message} on {channel}')
+    print(f'{username}: {user_message} on ({channel})')
+
+    if message.author == client.user:
+        return
     
+    if user_message.lower() == '1488':
+        response = 'фошист!'
+        await message.channel.send(response)
+    
+    if user_message.lower() == 'ты пидор' or 'ты пидор!':
+        response = 'А может ты пидор?!'
+        await message.channel.send(response)
+
+client.run(TOKEN)
